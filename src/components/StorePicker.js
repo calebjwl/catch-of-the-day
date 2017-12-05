31bab2c041c0746b3977d8c203727f057bfb7100
Caleb Lee <caleb.gwl@gmail.com>
2017-12-05 12:53:17 -0600

    Context type, transitionTo route

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   // super creates react component (including render) and lets us add more
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+     // grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`going to ${storeId}`);
+     // transition ro /store/:storeId
+     this.context.router.transitionTo(`/store/${storeId}`)
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()}/>
+          defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
+}
+
+// allows us to use transitionTo component
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
