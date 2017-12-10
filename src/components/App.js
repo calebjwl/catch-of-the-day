2ab805124dc6aac461a70139329316cba7bdb383
Caleb Lee <caleb.gwl@gmail.com>
2017-12-09 21:48:48 -0600

    Loop over list of fishes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   constructor() {
@@ -38,6 +39,15 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {
+              Object
+              // returns ["fish1", "fish2", etc...]
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+            }
+            <Fish/>
+          </ul>
         </div>
         <Order/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
