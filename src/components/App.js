ecc6338ab8863cc3c4367f4736dbfadf36667569
Caleb Lee <caleb.gwl@gmail.com>
2017-12-14 16:49:12 -0600

    removeFromOrder function working

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     // getInitialState
     this.state = {
       fishes: {},
@@ -88,6 +89,12 @@ class App extends React.Component {
     this.setState({ order });
   }
 
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -106,6 +113,7 @@ class App extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params}
+          removeFromOrder={this.removeFromOrder}
           />
         <Inventory
           addFish={this.addFish}
