309487284937522182c13f484ddb481d35e167a1
Caleb Lee <caleb.gwl@gmail.com>
2017-12-15 11:51:09 -0600

    renderLogin function

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -18,6 +18,21 @@ class Inventory extends React.Component {
     this.props.updateFish(key, updatedFish)
   }
 
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>
+          Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>
+          Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')}>
+          Log In with Twitter</button>
+      </nav>
+    )
+  }
+
   renderInventory(key) {
     const fish = this.props.fishes[key];
     return (
